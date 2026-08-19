6e738b642f9314424227df8fbaac5560197617fa
jimmynakatsu <jimmynakatsu@gmail.com>
2026-08-19 15:30:00 -0600

    Delete Paintext paper directory

--- Paintext paper/Plaintext scripts/collatz_anim.py
+++
@@ -1,6 +0,0 @@
-@misc{collatz_generalized_bounds_2026,
-  title={On the Bounded Growth, Streak Invariants, and Drift Thresholds of Generalized Collatz Mappings},
-  author={[Jimmy Nakatsu]},
-  year={2026},
-  howpublished={\url{[https://github.com/](https://github.com/)[jimmynakatsu]/generalized-collatz-bounds}}
-}
